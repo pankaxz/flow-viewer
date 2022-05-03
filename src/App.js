72857024cc5cc3591175028f98a13be27b14e507
painkazz <pankajpanwar3.98@gmail.com>
2022-05-04 01:44:01 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css'
 
 function App() {
-  return <div className="App">Main app</div>
+  return (
+    <div className="App">
+      Main app
+      <button>Create project </button>
+    </div>
+  )
 }
 
 export default App
